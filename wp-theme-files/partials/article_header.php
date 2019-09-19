c327b233bb0198023cb4795a4abc15772e55bfc8
childressdev <jcampbell@childressagency.com>
2019-09-19 18:58:10 -0400

    fix array

--- wp-theme-files/partials/article_header.php
+++ wp-theme-files/partials/article_header.php
@@ -2,18 +2,18 @@
   <?php
     $header = get_field('article_header');
     if($header){
-      if($header['image']){
+      if($header['article_header']['image']){
         printf(
           '<img src="%s" class="img-fluid d-block mb-4" alt="%s" />',
-          esc_url($header['image']['url']),
-          esc_attr($header['image']['alt'])
+          esc_url($header['article_header']['image']['url']),
+          esc_attr($header['article_header']['image']['alt'])
         );
       }
 
-      printf('<h1>%s</h1>', esc_html($header['title']));
+      printf('<h2>%s</h2>', esc_html($header['article_header']['title']));
 
-      if($header['subtitle']){
-        printf('<p class="subtitle">%s</p>', esc_html($header['subtitle']));
+      if($header['article_header']['subtitle']){
+        printf('<p class="subtitle">%s</p>', esc_html($header['article_header']['subtitle']));
       }
     }
   ?>
