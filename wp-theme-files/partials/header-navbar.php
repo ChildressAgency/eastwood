65c77a77fc7e858c813a9be185eab1c203e3e58f
childressdev <jcampbell@childressagency.com>
2019-10-07 16:07:42 -0400

    added check if sub_item exists

--- wp-theme-files/partials/header-navbar.php
+++ wp-theme-files/partials/header-navbar.php
@@ -68,21 +68,23 @@ if(!defined('ABSPATH')){ exit; }
                                   }
 
                                   $sub_item = $menu_sub_item['product_category'];
-                                  $sub_item_title = $sub_item->name;
-                                  $sub_item_slug = $sub_item->slug;
-                                  $sub_item_url = get_term_link($sub_item);
-                                  $sub_item_image = $menu_sub_item['menu_item_image']; 
+                                  if($sub_item){
+                                    $sub_item_title = $sub_item->name;
+                                    $sub_item_slug = $sub_item->slug;
+                                    $sub_item_url = get_term_link($sub_item);
+                                    $sub_item_image = $menu_sub_item['menu_item_image']; 
 
-                                  printf(
-                                    '<a href="%s" id="tab-%s-%s" class="nav-link" data-menu_img_target="menu-img-%s" data-menu_img="%s" data-menu_img_alt="%s">%s</a>',
-                                    esc_url($sub_item_url),
-                                    esc_attr($menu_item_slug),
-                                    esc_attr($sub_item_slug),
-                                    esc_attr($menu_item_slug),
-                                    esc_url($sub_item_image['url']),
-                                    esc_attr($sub_item_image['alt']),
-                                    esc_html($sub_item_title)
-                                  );
+                                    printf(
+                                      '<a href="%s" id="tab-%s-%s" class="nav-link" data-menu_img_target="menu-img-%s" data-menu_img="%s" data-menu_img_alt="%s">%s</a>',
+                                      esc_url($sub_item_url),
+                                      esc_attr($menu_item_slug),
+                                      esc_attr($sub_item_slug),
+                                      esc_attr($menu_item_slug),
+                                      esc_url($sub_item_image['url']),
+                                      esc_attr($sub_item_image['alt']),
+                                      esc_html($sub_item_title)
+                                    );
+                                  }
 
                                   $i++;  
                                 }
