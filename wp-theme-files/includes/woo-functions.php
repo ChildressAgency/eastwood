4cc2c480f344624a025bdbf51f1ba64d9ce8018c
childressdev <jcampbell@childressagency.com>
2019-11-12 15:25:09 -0500

    removed relate products display

--- wp-theme-files/includes/woo-functions.php
+++ wp-theme-files/includes/woo-functions.php
@@ -214,3 +214,5 @@ function eastwood_product_inquiry_modal(){
     </div>
   </div>
 <?php }
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
